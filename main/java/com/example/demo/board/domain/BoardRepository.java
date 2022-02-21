27f01757425f8efdb138cc9a5262348938695774
jennharw <73508138+jennharw@users.noreply.github.com>
2022-02-21 13:01:53 +0900

    Delete main/java/com/example/demo/board directory

--- main/java/com/example/demo/board/domain/BoardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.board.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-    List<Board> findAllByOrderByCreatedAtDesc();
-}
